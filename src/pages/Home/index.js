c48415d57d2581b60cb456c7b3b949afad14366c
Guillaume Bzh <guillaume4cebil@gmail.com>
2026-04-11 15:09:36 +0200

    fix(Events) bug twin cards fixed

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -77,7 +77,7 @@ const Page = () => {
             <footer className="row">
                 <div className="col presta">
                     <h3>Notre derniére prestation</h3>
-                    {last && <EventCard imageSrc={last?.cover} title={last?.title} date={new Date(last?.date)} small label={last?.type} />}
+                    {last && <EventCard imageSrc={last.cover} title={last.title} date={new Date(last.date)} small label={last.type} />}
                 </div>
                 <div className="col contact">
                     <h3>Contactez-nous</h3>
